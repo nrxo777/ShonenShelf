3128769cc5821198b330ea50610ec6c11fb76cfc
nrxo777 <noorxcalma@gmail.com>
2025-02-14 21:37:29 +0600

    making_responive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,54 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-    const [menu, setMenu] = useState("home");
+  const [menu, setMenu] = useState("home");
 
   return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>HOME</li>
-            <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>MENU</li>
-            <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>MOBILE APP</li>
-            <li onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>CONTACT US</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot">
-
-                </div>
-            </div>
-            <button>Sign in</button>
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          HOME
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          MENU
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          MOBILE APP
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          CONTACT US
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
+        <button>Sign in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
