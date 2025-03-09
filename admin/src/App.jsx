cafe107c7c80415e1ca9a2bb09cc722ff0cdd03c
nrxo777 <noorxcalma@gmail.com>
2025-03-10 00:29:15 +0600

    navbar_admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+
+const Sidebar = () => {
+  return <></>;
+};
+
+export default Sidebar;
