2ee4bfb2ebaa47b20b2b11b4e784d9a4c276cb6b
nrxo777 <noorxcalma@gmail.com>
2025-03-10 01:01:14 +0600

    sidebar_admin

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,26 @@ import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return <></>;
+  return (
+    <>
+      <div className="sidebar">
+        <div className="sidebar-options">
+          <div className="sidebar-option">
+            <img src={assets.add_icon} alt="" />
+            <p>Add Items</p>
+          </div>
+          <div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>List Items</p>
+          </div>
+          <div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>Orders Items</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
